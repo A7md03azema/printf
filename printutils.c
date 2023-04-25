2d9004465e105408857ee83b1ef7066f30f02345
A7md03azema <ahmedabdelaziem696@gmail.com>
2023-04-25 14:46:40 +0200

    last bug

--- printutils.c
+++ printutils.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * is_printable - Evaluates if a char is printable.
+ * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- * * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 if c is printable, 0 otherwise
  */
 
 int is_printable(char c)
@@ -17,7 +17,6 @@ int is_printable(char c)
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
- *
  * Return: Always 3
  */
 
@@ -39,7 +38,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 /**
  * is_digit - Verifies if a char is a digit
  * @c: Char to be evaluated
- * * Return: 1 if c is a digit, 0 otherwise
+ *
+ * Return: 1 if c is a digit, 0 otherwise
  */
 
 int is_digit(char c)
@@ -53,7 +53,6 @@ int is_digit(char c)
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
  * @size: Number indicating the type to be casted.
- *
  * Return: Casted value of num
  */
 
@@ -70,7 +69,6 @@ long int convert_size_number(long int num, int size)
  * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- *
  * Return: Casted value of num
  */
 
